9a83739e793798db3701ba0bbfbc694a9a89ff2d
Facucode <34733879+Facucode@users.noreply.github.com>
2021-04-01 09:47:29 -0300

    Primer commit del TP1

---
+++ tp.js
@@ -0,0 +1,42 @@
+/* 
+Realizar una función que permita contar la cantidad de vocales que tiene un texto que se recibe como parámetro. No habrá discriminación entre las vocales en mayúscula y en minúscula. Las vocales acentuadas no se contarán. El valor obtenido se retornará al terminar la función. Si se no se recibe un string retornará -1.
+*/
+function contarVocales(texto) {
+  let vocales = ["a","e","i","o","u"];
+  let cantVocales= 0;
+  if (typeof(texto) !== 'string'){
+    cantVocales=-1
+  }
+  else{
+  for(let i = 0; i < texto.length; i++ ){
+    if(vocales.includes(texto[i].toLowerCase()))
+    cantVocales++
+  }
+  
+    
+}
+return cantVocales;
+
+}
+ //console.log(contarVocales("AAAAaaaa"))
+
+/* 
+Crear un repo en github y subir todo el proyecto. Se ignorará la carpeta node_modules (para ellos está creado el archivo .gitignore en este proyecto) Esta función devolverá un string con la url del repo.
+*/
+function urlRepo() {
+  return
+}
+
+/* 
+Crear una función arrow, que devuelva una clase en ES6 que contendrá dos métodos llamados contadorPalabras y hayNumeros. La clase recibirá un texto que se guardará en una propiedad llamada texto. contadorPalabras retornará la cantidad de palabras encontradas en la propiedad texto y hayNumeros devolverá true en caso de que encuentre un número en dicho texto, caso contrario retorna false. En ambos métodos, si el texto no es válido, se devolverá -1
+Crear un propiedad estática contadorInstancias que me indique cuantas instancias hay de esa clase.
+*/
+const crearClase = () => {
+  return
+}
+
+module.exports = {
+  contarVocales,
+  urlRepo,
+  crearClase
+};
